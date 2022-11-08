97f26c135b52b50112b5565798be135f00a99646
mohiuddintanvir <mohiuddin.tanvir.mollah@gmail.com>
2022-11-09 04:18:01 +0600

    second

--- index.js
+++ index.js
@@ -16,12 +16,22 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.Db_user}:${process.env.Db_PASSWORD}@cluster0.oczpomj.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+async function run() {
+    try {
+        const serviceCollection = client.db('greenven').collection('services');
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        })
 
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err))
 
 
 
