945a29b58a0f2c1c71c672c3282965391c5305aa
mohiuddintanvir <mohiuddin.tanvir.mollah@gmail.com>
2022-11-10 18:14:47 +0600

    seven

--- index.js
+++ index.js
@@ -80,6 +80,12 @@ async function run() {
         })
 
         // U
+        app.get('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const user = await ordersCollection.findOne(query)
+            res.send(user);
+        })
         app.patch('/orders/:id', async (req, res) => {
             const id = req.params.id;
             const status = req.body.statusa
